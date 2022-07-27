dba3d81b8af9a14ddd0992004deb7189c689f715
Ryan Nemeth <ryan.nemeth@aprimo.com>
2022-07-27 05:55:33 -0400

    GeneratePBKDF2() is working

--- generator/generator.go
+++ generator/generator.go
@@ -24,7 +24,10 @@ func GenerateRandomString(length int64) (string, error) {
 
 // GeneratePBCKS5 generates a PBCKS5 compliant password
 func GeneratePBKDF2() (string, error) {
+	// example: trilogy3lectured5chubbiness1toads9breaths
+	rand.Seed(time.Now().Unix())
 	log.SetPrefix("GeneratePBKDF2():")
+
 	var builder strings.Builder
 
 	for i := 0; i <= 4; i++ {
@@ -32,17 +35,6 @@ func GeneratePBKDF2() (string, error) {
 		number := strconv.Itoa(getRandomNumber())
 		builder.WriteString(number)
 	}
-	// next, we need to build a string
-	// the string should have 5 random words,
-	// separated by 4 random numbers
-
-	// use getRandomWord() and getRandomInt()
-	// to generate a random string like:
-	// example: trilogy3lectured5chubbiness1toads9breaths
-	// we may use a stringbuilder for this
-
-	// generate a password using the PBKDF2 algorithm
-
 	return builder.String(), nil
 }
 
@@ -52,7 +44,6 @@ func generatePassword(input int64) (string, error) {
 	return base64.URLEncoding.EncodeToString(b), err
 }
 
-// This function returns (secure) random bytes
 func generateBytes(input int64) ([]byte, error) {
 	log.SetPrefix("generateBytes():")
 	b := make([]byte, input)
@@ -63,10 +54,8 @@ func generateBytes(input int64) ([]byte, error) {
 	return b, nil
 }
 
-// GetRandomWord does something
 func getRandomWord() string {
 
-	rand.Seed(time.Now().Unix())
 	var lines []string
 	// set our log prefix
 	log.SetPrefix("getRandomWord(): ")
@@ -79,21 +68,13 @@ func getRandomWord() string {
 
 	// split the []byte into []string
 	lines = strings.Split(string(words), "\n")
-
-	// get a random number, max len(lines)
 	n := rand.Intn(len(lines))
-
 	// return a random word
 	return lines[n]
 }
 
 func getRandomNumber() int {
-
 	log.SetPrefix("getRandomNumber(): ")
-
-	rand.Seed(time.Now().Unix())
-	// generate a random number and return it
 	n := rand.Intn(10)
-
 	return n
 }
